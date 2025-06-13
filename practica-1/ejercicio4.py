58527fea59bba61c5403d616b8336f8614fb7ed6
Enzo Ezequiel Ruiz <107582009+EnzoEzequielRuiz@users.noreply.github.com>
2025-06-13 17:59:42 +0000

    practica 1 ejercicio4

---
+++ practica-1/ejercicio4.py
@@ -0,0 +1,7 @@
+'''
+-Escribir una expresion matematica que dado un numero natural evalue al siguiente numero mas grande. Por ejemplo, dado el numero 1 el resultado debe evaluar la expresion 2, dado 37 el resultado es 38.
+-Escribir en papel un programa quie pida al usuario un valor entero y devuelva el siguiente numero mas grande.
+'''
+
+num = int(input('num: '))
+print(num+1)
