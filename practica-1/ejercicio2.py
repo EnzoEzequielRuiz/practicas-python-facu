760ddd8e5d6fb3bea17daac0c5bb338a1491d35d
Enzo Ezequiel Ruiz <107582009+EnzoEzequielRuiz@users.noreply.github.com>
2025-06-13 17:45:15 +0000

    practica 1 ejercicio 2 python

---
+++ practica-1/ejercicio2.py
@@ -0,0 +1,16 @@
+'''
+Dar expresiones de codigo tal cual como las escribiria en python para calcular las siguientes expresiones matematicas
+'''
+x = 3
+y = 6
+z = 7
+
+uno = x/y + 1
+
+dos = (x + y) / (x - y)
+
+tres = (x + (y / z) / (x - (y / z)))
+
+cuatro = ((x + y)**2)**2
+
+print(uno,dos,tres,cuatro)
