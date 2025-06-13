e4cc2d9cc10d7d213eac999c87a1b499529fa08d
Enzo Ezequiel Ruiz <107582009+EnzoEzequielRuiz@users.noreply.github.com>
2025-06-13 18:05:35 +0000

    practica-1 ejercicio-5 de pyton

---
+++ practica-1/ejercicio5.py
@@ -0,0 +1,8 @@
+'''
+-Escribir una expresion matematica que describa el promedio ente dos numeros.
+-Escribir en papel un programa que pida al usuario dos numeros y muestre por pantalla su promedio.
+'''
+
+x = float(input('num1: '))
+y = float(input('num2: '))
+print('El promedio es: ', (x+y)/2)
