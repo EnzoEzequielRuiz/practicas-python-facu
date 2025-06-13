8ffa9c79dbd1370934f1e6f5e2bacc7b9c69cda4
Enzo Ezequiel Ruiz <107582009+EnzoEzequielRuiz@users.noreply.github.com>
2025-06-13 17:54:44 +0000

    practica 1 ejercicio 3 python

---
+++ practica-1/ejercicio3.py
@@ -0,0 +1,9 @@
+'''
+-Escribir la expresion matematica que representa al resultado de la suma de dos numeros cualesquiera.
+-Escribir en papel un programa que pida al usuario dos valores cualquiera e imprima en pantalla la suma de ambos.
+'''
+
+# x + y
+x = float(input('num1: '))
+y = float(input('num2: '))
+print('La suma de: ',x,' y ',y,' es: ',x+y)
