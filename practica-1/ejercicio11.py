dcda58e20d3044bc0a11772678283443d80ffaae
Enzo Ezequiel Ruiz <107582009+EnzoEzequielRuiz@users.noreply.github.com>
2025-06-14 02:59:45 +0000

    practica-1 ejercicio-11

---
+++ practica-1/ejercicio11.py
@@ -0,0 +1,10 @@
+'''
+Escribir un programa en python que imprima la siguiente figura en la pantalla utilizando un solo print.
+*
+***
+*****
+*******
+*********
+'''
+
+print('*\n***\n*****\n*******\n*********')
